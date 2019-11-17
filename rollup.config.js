7401ebc44ea2c36fa85bd58a5dd0811114fe02d5
César Alberca <cesalberca@gmail.com>
2019-11-17 18:13:14 +0100

    Add parser and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,5 +2,10 @@ import typescript from "rollup-plugin-typescript";
 
 export default {
   input: "./src/main.ts",
+  output: {
+    esModule: false,
+    file: './dist/bundle.js',
+    format: 'cjs'
+  },
   plugins: [typescript()]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
-function hello() {
+export function main() {
     Logger.log('hi!')
 }
