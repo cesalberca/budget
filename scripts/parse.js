24a49254b80db837975c8922e403a53a61d58a2b
César Alberca <cesalberca@gmail.com>
2019-11-17 18:17:47 +0100

    Tweak declaration

--- scripts/parse.js
+++ scripts/parse.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const path = require('path')
 
 const data = fs.readFileSync(path.resolve(__dirname, '../dist/bundle.js'), 'utf-8');
-const exportsDeclaration = "exports.main = main";
+const exportsDeclaration = "exports.main = main;";
 const newValue = data.replace(new RegExp(exportsDeclaration), '');
 fs.writeFileSync(path.resolve(__dirname, '../dist/bundle.js'), newValue, 'utf-8');
