040d3cd50cffa6e3eb698d56ef46b7fe394b52c6
César Alberca <cesalberca@gmail.com>
2020-03-08 12:48:43 +0100

    Fix parse

--- scripts/parse.js
+++ scripts/parse.js
@@ -2,8 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 const data = fs.readFileSync(path.resolve(__dirname, '../dist/bundle.js'), 'utf-8')
-const exportsDeclarations = ['exports.main = main;', 'exports.calculateBalances = calculateBalances;']
-exportsDeclarations.forEach(exportDeclaration => {
-  const newValue = data.replace(new RegExp(exportDeclaration), '')
-  fs.writeFileSync(path.resolve(__dirname, '../dist/bundle.js'), newValue, 'utf-8')
-})
+const withoutMain = data.replace('exports.main = main;', '')
+const withoutBalances = withoutMain.replace('exports.calculateBalances = calculateBalances;', '')
+
+fs.writeFileSync(path.resolve(__dirname, '../dist/bundle.js'), withoutBalances, 'utf-8')
