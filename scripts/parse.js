0f3a48aac9ccf889805670d077f8c262343ad363
César Alberca <cesalberca@gmail.com>
2020-03-08 12:14:24 +0100

    Add parse calculate balances

--- scripts/parse.js
+++ scripts/parse.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const data = fs.readFileSync(path.resolve(__dirname, '../dist/bundle.js'), 'utf-8')
-const exportsDeclaration = 'exports.main = main;'
-const newValue = data.replace(new RegExp(exportsDeclaration), '')
-fs.writeFileSync(path.resolve(__dirname, '../dist/bundle.js'), newValue, 'utf-8')
+const exportsDeclarations = ['exports.main = main;', 'exports.calculateBalances = calculateBalances;']
+exportsDeclarations.forEach(exportDeclaration => {
+  const newValue = data.replace(new RegExp(exportDeclaration), '')
+  fs.writeFileSync(path.resolve(__dirname, '../dist/bundle.js'), newValue, 'utf-8')
+})
