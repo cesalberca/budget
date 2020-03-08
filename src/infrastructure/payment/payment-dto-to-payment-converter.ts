1ce1c5594e6595e1d2edf930ddd225e156bbca2d
César Alberca <cesalberca@gmail.com>
2020-03-08 14:38:00 +0100

    Fix conversion

--- src/infrastructure/payment/payment-dto-to-payment-converter.ts
+++ src/infrastructure/payment/payment-dto-to-payment-converter.ts
@@ -4,13 +4,16 @@ import { Datetime } from '../../domain/datetime'
 import { Payment } from '../../domain/payment/payment'
 
 export class PaymentDtoToPaymentConverter implements Converter<PaymentDto, Payment> {
-  convert([timestamp, detail, payer, debtor, type, quantity, date, owned]: PaymentDto): Payment {
+  convert([timestamp, detail, from, to, type, quantity, date, owned]: PaymentDto): Payment {
     return {
       timestamp: Datetime.fromIso(timestamp),
-      from: payer,
+      from,
       quantity,
       detail,
-      to: debtor.split(',').map(name => name.trim()),
+      to: to
+        .split(',')
+        .map(name => name.trim())
+        .filter(name => name !== ''),
       type,
       date: Datetime.fromIso(date),
       owned
