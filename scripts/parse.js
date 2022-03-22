f39b98bfedc54c95cc72f8fddab16bcded9f607f
César Alberca <cesalberca@gmail.com>
2022-03-22 14:14:31 +0100

    Delete scripts

--- scripts/parse.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const data = fs.readFileSync(path.resolve(__dirname, '../dist/bundle.js'), 'utf-8')
-const withoutMain = data.replace('exports.main = main;', '')
-const withoutBalances = withoutMain.replace('exports.calculateBalances = calculateBalances;', '')
-
-fs.writeFileSync(path.resolve(__dirname, '../dist/bundle.js'), withoutBalances, 'utf-8')
